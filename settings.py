6de3e1b13919b472b14c22ca8fc73830a89c8764
introverthink <97953204+introverthink@users.noreply.github.com>
2025-04-18 15:28:12 +0500

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,23 @@
+# shop_project/settings.py (добавить в существующий файл)
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'shop',  # наше приложение
+]
+
+# Настройки для медиа-файлов
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Настройки для статических файлов
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Настройка для перенаправления после логина
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = 'home'
